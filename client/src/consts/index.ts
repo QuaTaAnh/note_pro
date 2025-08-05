6c1132014a00581f7b773749feb16f7a7c61d63a
anh7bin <letrannhatanh171@gmail.com>
2025-08-05 16:23:36 +0700

    fix bug variable

--- client/src/consts/index.ts
+++ client/src/consts/index.ts
@@ -1,1 +1,9 @@
+import { ROUTES } from "@/lib/routes";
+
 export const AUTHENTICATED = "authenticated";
+export const PAGE_TITLES: Record<string, string> = {
+    [ROUTES.HOME]: "Home | Bin Craft",
+    [ROUTES.LOGIN]: "Login | Bin Craft",
+    [ROUTES.DEMO]: "UI Demo | Bin Craft",
+  };
+  
