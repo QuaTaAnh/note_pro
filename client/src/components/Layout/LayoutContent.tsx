ffe89e1b33fa1f0c8e28f4f889dba4e5ff90a58b
anh7bin <letrannhatanh171@gmail.com>
2025-08-05 15:59:43 +0700

    auto-document-title

--- client/src/components/Layout/LayoutContent.tsx
+++ client/src/components/Layout/LayoutContent.tsx
@@ -1,9 +1,11 @@
 "use client";
 
 import { usePathname } from "next/navigation";
+import { useEffect } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import { ROUTES } from "@/lib/routes";
+import { PAGE_TITLES } from "@/consts";
 
 export default function LayoutContent({
   children,
@@ -11,6 +13,11 @@ export default function LayoutContent({
   children: React.ReactNode;
 }) {
   const pathname = usePathname();
+
+  useEffect(() => {
+    const title = PAGE_TITLES[pathname] || "Bin Craft";
+    document.title = title;
+  }, [pathname]);
 
   return pathname === ROUTES.LOGIN ? (
     <>{children}</>
