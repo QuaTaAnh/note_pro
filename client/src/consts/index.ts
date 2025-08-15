c9686c6c93b2c3892316c4ca21c3eba963a58aa7
anh7bin <letrannhatanh171@gmail.com>
2025-08-15 16:47:03 +0700

    feature/editor-layout

--- client/src/consts/index.ts
+++ client/src/consts/index.ts
@@ -7,32 +7,34 @@ export const AUTHENTICATED = "authenticated";
 export const UNAUTHENTICATED = "unauthenticated";
 export const HEADER_HEIGHT = 48;
 export const SIDEBAR_WIDTH = 288;
+
+
 export const PAGE_TITLES: Record<string, string> = {
-  [ROUTES.HOME]: "Home",
-  [ROUTES.LOGIN]: "Login",
-  [ROUTES.DEMO]: "UI Demo",
+	[ROUTES.HOME]: "Home",
+	[ROUTES.LOGIN]: "Login",
+	[ROUTES.DEMO]: "UI Demo",
 };
 
 export const MENU_ITEMS = (workspaceSlug: string) => [
-    {
-      icon: FiFileText,
-      label: "All Docs",
-      href: ROUTES.WORKSPACE_ALL_DOCS(workspaceSlug),
-    },
-    {
-      icon: AiOutlineCheckCircle,
-      label: "Tasks",
-      href: ROUTES.WORKSPACE_TASKS(workspaceSlug),
-    },
-    {
-      icon: LuCalendarRange,
-      label: "Calendar",
-      href: ROUTES.WORKSPACE_CALENDAR(workspaceSlug),
-    },
-    {
-      icon: LuBookOpenText,
-      label: "My Templates",
-      href: ROUTES.WORKSPACE_TEMPLATES(workspaceSlug),
-    },
-  ];
+	{
+		icon: FiFileText,
+		label: "All Docs",
+		href: ROUTES.WORKSPACE_ALL_DOCS(workspaceSlug),
+	},
+	{
+		icon: AiOutlineCheckCircle,
+		label: "Tasks",
+		href: ROUTES.WORKSPACE_TASKS(workspaceSlug),
+	},
+	{
+		icon: LuCalendarRange,
+		label: "Calendar",
+		href: ROUTES.WORKSPACE_CALENDAR(workspaceSlug),
+	},
+	{
+		icon: LuBookOpenText,
+		label: "My Templates",
+		href: ROUTES.WORKSPACE_TEMPLATES(workspaceSlug),
+	},
+];
   
