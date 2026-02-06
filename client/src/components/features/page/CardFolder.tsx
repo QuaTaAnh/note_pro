310bf7ada9a464bd6cce1a955c450d38d2059fe1
anh7bin <letrannhatanh171@gmail.com>
2026-02-06 15:53:09 +0700

    add right click menu to card folder in folder page

--- client/src/components/features/page/CardFolder.tsx
+++ client/src/components/features/page/CardFolder.tsx
@@ -6,14 +6,14 @@ import {
     CardHeader,
     CardTitle,
 } from '@/components/ui/card';
-import { useWorkspace } from '@/hooks/useWorkspace';
 import { useLoading } from '@/contexts/LoadingContext';
+import { GetFolderByIdQuery } from '@/graphql/queries/__generated__/folder.generated';
+import { useWorkspace } from '@/hooks/useWorkspace';
 import { ROUTES } from '@/lib/routes';
-import { formatDate } from '@/lib/utils';
 import { Folder } from 'lucide-react';
 import { useRouter } from 'next/navigation';
 import React from 'react';
-import { GetFolderByIdQuery } from '@/graphql/queries/__generated__/folder.generated';
+import { FolderMoreMenu } from './FolderMoreMenu';
 
 type FolderType = NonNullable<
     GetFolderByIdQuery['folders_by_pk']
@@ -40,30 +40,38 @@ const CardFolderComponent = ({ folder }: CardFolderProps) => {
     };
 
     return (
-        <Card
-            key={folder.id}
-            className="group relative cursor-pointer transition-all duration-200 h-[140px] w-full rounded-md border-[rgb(223,228,231)] hover:border-primary dark:border-border dark:hover:border-primary flex flex-col"
-            onClick={handleClick}>
-            <CardHeader className="flex flex-col p-4 flex-1">
-                <div className="flex flex-col justify-center items-center gap-3">
-                    <div className="flex-shrink-0 w-10 h-10 rounded-lg bg-primary/10 flex items-center justify-center">
-                        {folder.icon ? (
-                            <span className="text-2xl">{folder.icon}</span>
-                        ) : (
-                            <Folder className="w-5 h-5 text-primary" />
-                        )}
+        <FolderMoreMenu
+            folder={{
+                ...folder,
+                description: folder.description ?? undefined,
+                icon: folder.icon ?? undefined,
+            }}>
+            <Card
+                key={folder.id}
+                className="group relative cursor-pointer transition-all duration-200 h-[140px] w-full rounded-md border-[rgb(223,228,231)] hover:border-primary dark:border-border dark:hover:border-primary flex flex-col"
+                onClick={handleClick}>
+                <CardHeader className="flex flex-col p-4 flex-1">
+                    <div className="flex flex-col justify-center items-center gap-3">
+                        <div className="flex-shrink-0 w-10 h-10 rounded-lg bg-primary/10 flex items-center justify-center">
+                            {folder.icon ? (
+                                <span className="text-2xl">{folder.icon}</span>
+                            ) : (
+                                <Folder className="w-5 h-5 text-primary" />
+                            )}
+                        </div>
+                        <div className="flex flex-col items-center gap-1">
+                            <CardTitle className="text-sm truncate">
+                                {folder.name}
+                            </CardTitle>
+                            <CardDescription className="text-xs">
+                                {docCount}{' '}
+                                {docCount <= 1 ? 'document' : 'documents'}
+                            </CardDescription>
+                        </div>
                     </div>
-                    <div className="flex flex-col items-center gap-1">
-                        <CardTitle className="text-sm truncate">
-                            {folder.name}
-                        </CardTitle>
-                        <CardDescription className="text-xs">
-                            {docCount} {docCount < 1 ? 'document' : 'documents'}
-                        </CardDescription>
-                    </div>
-                </div>
-            </CardHeader>
-        </Card>
+                </CardHeader>
+            </Card>
+        </FolderMoreMenu>
     );
 };
 
